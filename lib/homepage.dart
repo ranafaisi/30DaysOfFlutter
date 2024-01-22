603febb0fcb15c7e5e10c59532eef71c7d353845
Muhammad Faisal <you@example.com>
2024-01-22 18:33:00 +0500

    Day 2 completed

---
+++ lib/homepage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Catalog App')),
+      body: const Center(
+        child: Text(
+          "Welcome to App",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 40.0,
+          ),
+        ),
+      ),
+      drawer: const Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:tutorial_app/homepage.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Material(
-        child: Center(child: Text('Welcome to Flutter')),
-      ),
+      home: HomePage(),
     );
   }
 }
